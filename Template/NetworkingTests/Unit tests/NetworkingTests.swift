5a73e56bc8d1c449082c65ba5ff6a38d862b07b6
Domagoj Kulundžić <domagoj@martian.agency>
2019-05-27 12:27:41 +0200

    Added the Networking framework.

---
+++ Template/NetworkingTests/Unit tests/NetworkingTests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingTests.swift
+//  NetworkingTests
+//
+//  Created by Domagoj Kulundzic on 27/05/2019.
+//  Copyright © 2019 Martian & Machine. All rights reserved.
+//
+
+import XCTest
+@testable import Networking
+
+class NetworkingTests: XCTestCase {
+  override func setUp() { }
+  
+  override func tearDown() { }
+}
