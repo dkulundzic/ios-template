7f2e62f6d10233bec7e29082993aac7db024a79e
Domagoj Kulundžić <domagoj@martian.agency>
2019-05-27 12:28:26 +0200

    Added a placeholder UIViewController as the window's root view controller.

--- Template/Template/App/TemplateApp.swift
+++ Template/Template/App/TemplateApp.swift
@@ -14,6 +14,7 @@ class TemplateApp: UIResponder, UIApplicationDelegate {
   let window = UIWindow(frame: UIScreen.main.bounds)
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window.rootViewController = UIViewController()
     window.makeKeyAndVisible()
     return true
   }
